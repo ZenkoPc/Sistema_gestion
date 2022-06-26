4b1c4e8ec6d9e7f65cb8cc36883f3d778134bf40
Zenko_Pc <pipeobandog@hotmail.com>
2022-06-26 18:44:42 -0500

    Intento 4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2ddf8dd090c5af84c3cde6b9d2a756dcfc175ce6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2ddf8dd090c5af84c3cde6b9d2a756dcfc175ce6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
