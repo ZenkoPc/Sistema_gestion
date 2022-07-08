3c5176e6d26ba63df4103d53b012dc5cac0e0078
Zenko_Pc <pipeobandog@hotmail.com>
2022-07-08 16:08:19 -0500

    Sitio web

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-$app->get('login.php', function() use($app) {
-    $app['monolog']->addDebug('cowsay');
-    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-  });
+require once("login.php");
 
 ?>
